da263e9481e06e85e3665ea26f338d00bd6b6d82
jabbajunior <keiran.singh@gmail.com>
2026-04-23 12:52:58 -0400

    feat: add centralized logging and ci status reporting in ci.sh

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -5,34 +5,30 @@
 # Linting
 # Testing 
 # Merge PR
-# Not sure if have to build a new executable for docker image
-# Build new docker image
 
-SCRIPT_PATH="${BASH_SOURCE[0]}" # Grabs the current relative path of this script
-SCRIPT_DIR="$(cd "$(dirname "$SCRIPT_PATH")" && pwd)" # Gets the absolute path of the scripts folder
-PROJECT_ROOT="$(dirname "$SCRIPT_DIR")" # Gets the absolute path of the root project folder
+# Import in logging script
+source "scripts/logging.sh" || {
+    printf 'FATAL: %s\n' "$*" >&2
+    exit 1
+}
 
-cd $PROJECT_ROOT
+LOG_FILE=$1
+SCRIPT_PATH=$0
 
-# This can probably be better done with a positional parameter
-
-
+log "INFO" "has started execution"
 
 # Linting
-if ! uv run ruff check "$PROJECT_ROOT/app"; then
-    echo "$(date): linting failed" >> "$PROJECT_ROOT/logs/pipeline.log" # TODO: Replace this later with a centralized logger
-    echo "Linting failed!"
-    exit 1 # 1 = Linting
+if ! uv run ruff check "app/"; then
+    log "FATAL" "Linting Failed!"
+    exit 2 # 2 = Linting
 fi
+
+log "INFO" "Linting passed"
 
 # Testing
 if ! uv run pytest; then
-    echo "$(date): tests failed" >> "$PROJECT_ROOT/logs/pipeline.log" # TODO: Replace this later with a centralized logger
-    echo "Tests failed!"
+    log "FATAL" "Tests Failed!"
     exit 2 # 2 = Testing
 fi
 
-
-
-# would want the script to be resilient not dependent on you running it in a certain directory
-# pipeline.sh 
+log "INFO" "Tests passed"
